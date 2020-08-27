6543813ee7b27fe92453f4dc2e61ba87e97168e2
kps-1998 <53467385+kps-1998@users.noreply.github.com>
2020-08-27 10:57:03 +0530

    Create futureInterface.go

---
+++ futureInterface.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  "time"
+)
+
+type Future Interface{
+     get() Result
+     getWithTimeout(timeout time.Duration) Result 
+     isDone() bool
+     isCancelled() bool
+     cancel(mayInterruptIfRunning bool)
+}
